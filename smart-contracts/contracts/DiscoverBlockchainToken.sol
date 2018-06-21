00e609f440d94eef3b6696698d3e2f6e88f3c7fd
Aleksandar Djordjevic <adjordjevic@firstbeatmedia.com>
2018-06-22 01:21:05 +0200

    Adding new version of DiscoverBlockchainCrowdsale

--- smart-contracts/contracts/DiscoverBlockchainToken.sol
+++ smart-contracts/contracts/DiscoverBlockchainToken.sol
@@ -16,6 +16,9 @@ contract DiscoverBlockchainToken is Ownable, BurnableToken, StandardToken {
     uint8 public constant decimals = 18; // DSC decimal number
     uint256 public constant TOTAL_SUPPLY = 500000000 * (10 ** uint256(decimals)); // total amount of all DSC tokens - 500 000 000 DSC
 
+    /**
+     * @dev DiscoverBlockchainToken constructor, sets total supply and assigns total supply to owner
+     */
     constructor() public {
         totalSupply_ = TOTAL_SUPPLY; // set total amount of tokens
         balances[owner] = TOTAL_SUPPLY; // transfer all tokens to smart contract owner
